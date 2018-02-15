a093463001ae390692862f1c69d95c75dd29c89f
zrowe <paul.louis.rowe@gmailcom>
2018-02-14 22:45:20 -0800

    adding error handling for movies and songs not found

--- liri.js
+++ liri.js
@@ -41,7 +41,6 @@ function execute(command, argument) {
     }
 }
 
-
 // my-tweets
 // Show my last 20 tweets and when they were created.
 function myTweets() {
@@ -55,31 +54,30 @@ function myTweets() {
     });
 }
 
-
 // spotify-this-song '<song name here>'
 // Show information about the song.
 function spotifyThisSong(song) {
 
-    // Artist(s)
-    // The song's name
-    // A preview link of the song from Spotify
-    // The album that the song is from
-
     spotify
         .search({ type: 'track', query: song, limit: 1 })
-        .then(function(response) {
-            console.log("\n");
-            console.log(showArtists(response.tracks.items[0].album.artists));
-            console.log("Song's Name: " + response.tracks.items[0].name);
-            console.log("Preview Link: " + response.tracks.items[0].preview_url);
-            console.log("Album Name: " + response.tracks.items[0].album.name);
 
-            function showArtists(arr) {
-                var artists = "Artist(s): "
-                for (var i = 0; i < arr.length; i++) {
-                    artists = artists + arr[i].name + ", ";
+        .then(function(response) {
+            if (!(Array.isArray(response.tracks.items) && response.tracks.items.length)) {
+                console.log('Your song "' + song + '" was not found');
+            } else {
+                console.log(""); // for a blank line at start
+                console.log(showArtists(response.tracks.items[0].album.artists));
+                console.log("Song's Name: " + response.tracks.items[0].name);
+                console.log("Preview Link: " + response.tracks.items[0].preview_url);
+                console.log("Album Name: " + response.tracks.items[0].album.name);
+
+                function showArtists(arr) {
+                    var artists = "Artist(s): "
+                    for (var i = 0; i < arr.length; i++) {
+                        artists = artists + arr[i].name + ", ";
+                    }
+                    return artists.substr(0, artists.length - 2);
                 }
-                return artists.substr(0, artists.length-2);
             }
         })
         .catch(function(err) {
@@ -88,33 +86,41 @@ function spotifyThisSong(song) {
 }
 
 
+
 // movie-this '<movie name here>'
 // Output movie information to your terminal/bash window:
 function movieThis(movie) {
 
     request("https://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy", function(error, response, body) {
         if (!error && response.statusCode === 200) {
-            console.log("\nThe movie's title is: " + JSON.parse(body).Title);
-            console.log("It came out in: " + JSON.parse(body).Year);
-            console.log("IMDB rated it: " + JSON.parse(body).imdbRating);
-            console.log("Rotten Tomatoes rated it: " + searchRatings("Rotten Tomatoes", JSON.parse(body).Ratings));
-            console.log("It was produced in: " + JSON.parse(body).Country);
-            console.log("And the language is: " + JSON.parse(body).Language);
-            console.log("The actors: " + JSON.parse(body).Actors);
-            console.log("And here's the plot: " + JSON.parse(body).Plot);
 
-            function searchRatings(nameKey, myArray) {
-                for (var i = 0; i < myArray.length; i++) {
-                    if (myArray[i].Source === nameKey) {
-                        return myArray[i].Value;
+            var parsedBody = JSON.parse(body);
+            if (parsedBody.Response === "True") {
+                console.log("\nThe movie's title is: " + parsedBody.Title);
+                console.log("It came out in: " + parsedBody.Year);
+                console.log("IMDB rated it: " + parsedBody.imdbRating);
+                console.log("Rotten Tomatoes rated it: " + searchRatings("Rotten Tomatoes", parsedBody.Ratings));
+                console.log("It was produced in: " + parsedBody.Country);
+                console.log("And the language is: " + parsedBody.Language);
+                console.log("The actors: " + parsedBody.Actors);
+                console.log("And here's the plot: " + parsedBody.Plot);
+
+                function searchRatings(nameKey, myArray) {
+                    for (var i = 0; i < myArray.length; i++) {
+                        if (myArray[i].Source === nameKey) {
+                            return myArray[i].Value;
+                        }
                     }
+                    return "Not Rated";
                 }
-                return "Not Rated";
+            } else {
+                if (parsedBody.Response === "False") {
+                    console.log(parsedBody.Error);
+                }
             }
         }
     });
 }
-
 
 // node liri.js do-what-it-says
 // Take the text inside of random.txt and then use it to call one of LIRI's commands.
